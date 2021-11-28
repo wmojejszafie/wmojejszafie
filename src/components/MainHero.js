105c02c80521442d4e44071bd189e03b545ca0f4
wmojejszafie <wmojejszafie@gmail.com>
2021-11-28 23:17:12 +0100

    Adjust image for baner

--- src/components/MainHero.js
+++ src/components/MainHero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import mainHeroBaner from '../images/mainHeroBarSmallTitle.jpg'
+import mainHeroBaner from '../images/mainHeroBar.jpg'
 
 const Wrapper = styled.section`
   position: relative;
@@ -9,64 +9,49 @@ const Wrapper = styled.section`
   @media (min-width: ${props => props.theme.responsive.small}) {
     height: ${props => props.height || 'auto'};
   }
+  margin: 0 auto;
+  width: 100%;
+  max-width: ${props => props.theme.sizes.maxWidth};
 `
 
 const Title = styled.h1`
-  align-self: center;
-  font-size: 3em;
-  font-weight: 600;
-  width: 100%;
-  max-width: ${props => props.theme.sizes.maxWidth};
+  z-index: 2;
+  font-size: 1em;
+  top: 40%;
+  @media (min-width: ${props => props.theme.responsive.small}) {
+    font-size: 2em;
+    top: 50%;
+  }
+  font-weight: 200;
+  position: absolute;
+  width: 50%;
+  max-width: ${props => props.theme.sizes.maxWidthCentered};
   padding: 0 1rem;
-  top: 50%;
-  left: 50%;
+  left: 30%;
+  transform: translate(-50%, -50%);
   text-align: center;
-  color: black;
-  font-style: italic;
-`
-
-const SubTitleSpan = styled.span`
-  align-self: center;
-  justify-content: center;
-  max-width: ${props => props.theme.sizes.maxWidth};
-  margin-top: 20px;
-`
-
-const SubTitle = styled.h2`
-  align-self: flex-end;
-  font-size: 2em;
-  font-weight: 600;
+  color: white;
   font-style: italic;
 `
 
 const Image = styled.img`
   position: absolute;
   object-fit: cover;
+  object-position: center;
   width: 100%;
   height: 100%;
-  &::before {
-    content: '';
-    background: rgba(0, 0, 0, 0.25);
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    height: 100%;
-    width: 100%;
-    z-index: 1;
+  @media (min-width: ${props => props.theme.responsive.small}) {
   }
 `
 
 const MainHero = props => (
   <Wrapper>
-    {/* <Title>
-      W mojej szafie - blog o życiu, emocjach, kobiecości i własnej drodze.
-    </Title>
-    <SubTitleSpan>
-      <SubTitle>jak pokochać siebie</SubTitle>
-    </SubTitleSpan> */}
     <Image src={mainHeroBaner} />
+    <Title>
+      <p>{`W mojej szafie `}</p>
+      <p>{`- w poszukiwaniu własnej drogi`}</p>
+    </Title>
+    <Title></Title>
   </Wrapper>
 )
 
